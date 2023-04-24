1fae49035687a82a271bdafdccf7478d32875999
Yidu-xSkript <yidu95@gmail.com>
2023-04-24 17:12:38 +0300

    pep8 violation fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,16 +5,16 @@ Pascal's Triangle
 
 
 def pascal_triangle(n):
-    """Create a function def pascal_triangle(n): that returns a list of
-    lists of integers representing the Pascal's triangle of n
+    """returns a list of lists of integers representing
+    the Pascal's triangle of n
     """
     triangle = []
     if n > 0:
         for i in range(1, n + 1):
             level = []
-            current = 1
+            curr = 1
             for j in range(1, i + 1):
-                level.append(current)
-                current = current * (i - j) // j
+                level.append(curr)
+                curr = curr * (i - j) // j
             triangle.append(level)
     return triangle
