820776756794154bc9cf7eac1f473a9599544595
Yidu-xSkript <yidu95@gmail.com>
2023-06-08 21:49:04 +0300

    rotate metrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+2D Matrix - Rotate
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+    Args:
+        matrix (list[[list]]): a matrix
+    """
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[x][i]
+            matrix[x][i] = matrix[y][x]
+            matrix[y][x] = matrix[j][y]
+            matrix[j][y] = tmp
